331ead89d3496cbf32e889c4c5762dcfcd969e8f
lenchq <tirr06@gmail.com>
2025-03-27 22:07:25 +0500

    refactor: rename game1 to game

---
+++ RiconRico/Game.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RiconRico;
+
+public class Game : Microsoft.Xna.Framework.Game
+{
+    Texture2D ballTexture;
+    (int ballX, int ballY) ballPos = (0,0);
+    
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        ballTexture = Content.Load<Texture2D>("sprites/ball");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        {
+            ballPos.ballX += 1;
+        }
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(ballTexture, new Vector2(ballPos.ballX, ballPos.ballY), Color.White);
+        _spriteBatch.End();
+
+
+        base.Draw(gameTime);
+    }
+}
--- RiconRico/Program.cs
+++ RiconRico/Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new RiconRico.Game1();
+﻿using var game = new RiconRico.Game();
 game.Run();
