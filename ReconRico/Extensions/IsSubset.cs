ce0f3b13bb8246c396cbec5d4ed3d73be554cf90
lenchq <tirr06@gmail.com>
2025-05-06 18:33:00 +0500

    OPTIMIZE IsSubset.cs

--- ReconRico/Extensions/IsSubset.cs
+++ ReconRico/Extensions/IsSubset.cs
@@ -4,6 +4,6 @@ public static class IsSubset
 {
     public static bool IsSubsetOf<T>(this IEnumerable<T> source, IEnumerable<T> target)
     {
-        return source.All(target.Contains);
+        return !source.Except(target).Any();
     }
 }
